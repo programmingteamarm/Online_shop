93e1bfe7410ec65418a712e8d912d43c89d23a58
aregv <programming.team.arm@gmail.com>
2021-08-25 22:22:41 +0400

    hy

--- Final_project/core/urls.py
+++ Final_project/core/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from core import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('brand/', views.brand, name='brand'),
+    path('contact/', views.contact, name='contact')
 ]
--- Final_project/core/views.py
+++ Final_project/core/views.py
@@ -3,4 +3,16 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'core/home.html')
+
+
+def about(request):
+    return render(request, 'core/about.html')
+
+
+def brand(request):
+    return render(request, 'core/brand.html')
+
+
+def contact(request):
+    return render(request, 'core/contact.html')
 # Create your views here.
